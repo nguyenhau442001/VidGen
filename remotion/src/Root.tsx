13aab1af7c29fb039e88508ce6fa2860a8c85055
Hau Nguyen Ngoc <nguyenhau442001@gmail.com>
2026-06-28 22:01:43 +0700

    feat: derive composition dimensions and default props from render_manifest.json

--- remotion/src/Root.tsx
+++ remotion/src/Root.tsx
@@ -3,6 +3,7 @@ import { Composition, continueRender, delayRender } from "remotion";
 import { TikTokVideo } from "./TikTokVideo";
 import { RenderManifest } from "./types";
 import { waitForInter, waitForJetBrainsMono } from "./styles";
+import defaultManifest from "../../output/render_manifest.json";
 
 // Load fonts before any frame is captured
 const fontHandle = delayRender("Loading fonts");
@@ -10,21 +11,22 @@ Promise.all([waitForInter(), waitForJetBrainsMono()]).then(() => {
   continueRender(fontHandle);
 });
 
-const EMPTY_MANIFEST: RenderManifest = { fps: 30, width: 1080, height: 1920, scenes: [] };
-
 export const Root: React.FC = () => {
   return (
     <Composition
       id="TikTokVideo"
       component={TikTokVideo}
       durationInFrames={1}
-      fps={30}
-      width={1080}
-      height={1920}
-      defaultProps={{ manifest: EMPTY_MANIFEST }}
+      fps={defaultManifest.fps}
+      width={defaultManifest.width}
+      height={defaultManifest.height}
+      defaultProps={{ manifest: defaultManifest as RenderManifest }}
       calculateMetadata={async ({ props }) => {
         const manifest = props.manifest as RenderManifest;
         return {
+          fps: manifest.fps,
+          width: manifest.width,
+          height: manifest.height,
           durationInFrames: Math.max(1, manifest.scenes.reduce((s, sc) => s + sc.durationInFrames, 0)),
         };
       }}
